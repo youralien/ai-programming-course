a74b7b0e81e5db6538207062b18c0d32679a9fef
youralien <Ryan.Louie@students.olin.edu>
2017-09-18 14:32:08 -0500

    Removed most comments

--- ex/ch2.lisp
+++ ex/ch2.lisp
@@ -1,55 +1,37 @@
-;;; Do excercises 4, 7, 8, 9
-;;; Using names greater, has-list-p, print-dots, get-a-count, summit
-;;; I had only read up to 2.8 when trying these, as a warm down excercise for bed'
-
-;;; Define a function that takes two arguments and returns the greater of the
 (defun greater (x y)
   (if (> x y)
       x
     y))
 
-;;; define a function that takes a list as an argument and returns true if one of its elements is a list'
 (defun has-list-p (lst)
   (cond ((null lst) nil)
         ((listp (car lst)) t)
         (t (has-list-p (cdr lst)))))
-;;; old way
-;;;(defun has-list-p (lst)
-;;;  (if (null lst)
-;;;      nil
-;;;    (if (listp (car lst))
-;;;        t
-;;;      (has-list-p (cdr lst)))))
 
-;;; takes a positive integer and prints that many dots
-;;; iterative
 (defun print-dots (n)
   (do ((i 1 (1+ i)))
       ((> i n) nil)
     (format t ".")))
 
-;;; recursive
 (defun print-dots (n)
   (cond ((< n 1) nil)
         (t (format t ".") (print-dots (1- n) ))))
 
-;;; takes a list and returns the number of times the symbol `a` occurs in it
-;;; iterative
 (defun nth-is-a (n lst)
   (if (eql 'a (nth n lst))
       1
     0))
+
 (defun get-a-count (lst)
   (do ((i 0 (1+ i))
        (count 0 (+ (nth-is-a i lst) count)))
       ((>= i (length lst)) count)))
-    
 
-;;; recursive
 (defun head-is-a (lst)
   (if (eql (car lst) 'a)
       1
     0))
+
 (defun get-a-count (lst)
   (cond ((null lst) 0)
         (t (+ (get-a-count (cdr lst)) (head-is-a lst)))))
@@ -76,7 +58,9 @@
           ((and (null x) (not (null (cdr lst)))) (summit (cdr lst)))
           (t (+ x (summit (cdr lst)))))))
 
-;;; HOW ARE THESE DIFFERENT?!?!
+;;; HOW ARE THESE FUNCTIONS DIFFERENT?
+;;; The first works, the second does not
+;;; i.e. (summit '(1 2 3))
 ;;;(defun summit (lst)
 ;;;  (let ((x (car lst)))
 ;;;    (cond ((null x) 0)
