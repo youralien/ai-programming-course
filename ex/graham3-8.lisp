2bd0eee624288280e3830b097361a34cadd03b45
youralien <Ryan.Louie@students.olin.edu>
2017-09-21 01:20:51 -0500

    passed critique for show-dots

--- ex/graham3-8.lisp
+++ ex/graham3-8.lisp
@@ -2,19 +2,14 @@
   (show-dots-helper lst 0))
 
 (defun show-dots-helper (lst n)
-  (cond ((null lst) (progn
-                      (format t "NIL")
-                      (do ((i 0 (1+ i)))
-                          ((= i n))
-                        (format t ")"))))
-        ((not (listp lst)) (format t "~A)" lst))
-        ((listp (car lst)) (progn
-                             (format t "(")
-                             (show-dots-helper (car lst) 0)
-                             (format t " . ")
-                             (show-dots-helper (cdr lst) (1+ n))))
-        (t (progn
-             (format t "(~A . " (car lst))
-             (show-dots-helper (cdr lst) (1+ n))))))
+  (cond ((or (null lst) (not (listp lst))) (format t "~A" lst)
+         (do ((i 0 (1+ i)))
+             ((= i n))
+           (format t ")")))
+        ((listp (car lst)) (format t "(")
+         (show-dots-helper (car lst) 0)
+         (format t " . ")
+         (show-dots-helper (cdr lst) (1+ n)))
+        (t (format t "(~A . " (car lst))
+           (show-dots-helper (cdr lst) (1+ n)))))
 
-  
