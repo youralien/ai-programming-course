f47429231ab08d6b004066d4c7105e8d68790b75
youralien <Ryan.Louie@students.olin.edu>
2017-09-21 08:47:49 -0500

    Passed test cases except for . in show-list

--- ex/graham3-8.lisp
+++ ex/graham3-8.lisp
@@ -13,3 +13,25 @@
         (t (format t "(~A . " (car lst))
            (show-dots-helper (cdr lst) (1+ n)))))
 
+(defun show-list (lst)
+  (show-new-list lst))
+
+(defun show-new-list (lst)
+  (cond ((or (null lst) (not (listp lst))) (format t "~A" lst))
+        (t (format t "[")
+           (show-continued-list lst)
+           (format t "]"))))
+
+(defun show-continued-list (lst)
+  (cond ((not (listp lst)) (format t "~A" lst))
+        ((and (listp (car lst)) (null (cdr lst)))
+         (show-new-list (car lst)))
+        ((and (listp (car lst)) (not (null (cdr lst))))
+         (show-new-list (car lst))
+         (format t " ")
+         (show-continued-list (cdr lst)))
+        ((and (not (listp (car lst))) (null (cdr lst)))
+         (format t "~A" (car lst)))
+        (t
+         (format t "~A " (car lst))
+         (show-continued-list (cdr lst)))))
