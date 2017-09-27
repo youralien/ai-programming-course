61fffbf732906fdfbb2b1f5be12d3ba6678ee48e
youralien <ryanlouie2021@u.northwestern.edu>
2017-09-27 17:49:35 -0500

    Rewrote show-list to be more concise; passes unit tests

--- ex/graham3-8.lisp
+++ ex/graham3-8.lisp
@@ -7,25 +7,14 @@
            (format t ")"))))
 
 (defun show-list (lst)
-  (show-new-list lst))
-
-(defun show-new-list (lst)
-  (cond ((or (null lst) (not (listp lst))) (format t "~A" lst))
+  (cond ((atom lst) (format t "~A" lst))
         (t (format t "[")
-           (show-continued-list lst)
-           (format t "]"))))
-
-(defun show-rest-list (lst)
- (unless (null (cdr lst))
-   (format t " ")
-   (unless (listp (cdr lst))
-     (format t ". "))
-   (show-continued-list (cdr lst))))
-
-(defun show-continued-list (lst)
-  (cond ((not (listp lst)) (format t "~A" lst)) ; 'NIL, 12'
-        ((listp (car lst)) (show-new-list (car lst))
-         (show-rest-list lst))
-        (t (format t "~A" (car lst))
-           (show-rest-list lst))))
-
+           (cond ((atom (car lst)) (format t "~A" (car lst)))
+                 (t (show-list (car lst))))
+           (do ((rest (cdr lst) (cdr rest)))
+                ((atom rest) (unless (null rest)
+                               (format t " . ~A" rest))
+                 (format t "]"))
+               (cond ((atom (car rest)) (format t " ~A" (car rest)))
+                     (t (format t " ")
+                        (show-list (car rest))))))))
