0b9167534dd2dfca5013e74abe1fde7856a079fb
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-11 21:18:44 -0500

    Fixed cyclic loops on bfs

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -1,15 +1,17 @@
 (defun shortest-path (start end net)
-  (bfs end (list (list start)) net))
+  (catch 'abort
+    (bfs end (list (list start)) '() net)))
 
-(defun bfs (end queue net)
+(defun bfs (end queue seen net)
   (if (empty-queue-p queue)
     nil
     (let ((path (car queue)))
       (let ((node (car path)))
-        (if (eql node end) (reverse path)
-          (bfs end
-            (append (cdr queue)
-              (new-paths path node net)) net))))))
+        (cond ((eql node end) (throw 'abort (reverse path)))
+              ((not (null (member node seen))) (throw 'abort nil))
+              (t (bfs end 
+                    (append (cdr queue)
+                      (new-paths path node net)) (append seen (list node)) net)))))))
 
 (defun new-paths (path node net)
   (mapcar #'(lambda (n)
