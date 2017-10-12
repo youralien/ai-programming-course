77f01be35cd437ef64ad53d1637e291295b705d4
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-11 22:08:02 -0500

    cyclical loops definitely are not being added

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -7,11 +7,15 @@
     nil
     (let ((path (car queue)))
       (let ((node (car path)))
-        (cond ((eql node end) (throw 'abort (reverse path)))
-              ((not (null (member node seen))) (throw 'abort nil))
-              (t (bfs end 
-                    (append (cdr queue)
-                      (new-paths path node net)) (append seen (list node)) net)))))))
+        (cond ((eql node end) (reverse path))
+              ; ((not (null (member node seen))) (throw 'abort nil))
+              (t (let ((nextpaths (new-paths path node net))
+                       (newqueue (cdr queue)))
+                   (dolist (npath nextpaths path)                   
+                     (if (null (some (lambda (n) (member n seen)) npath)) 
+                         (throw 'abort nil)
+                       (setq newqueue (append newqueue (list npath)))))
+                   (bfs end newqueue (append seen (list node)) net))))))))
 
 (defun new-paths (path node net)
   (mapcar #'(lambda (n)
