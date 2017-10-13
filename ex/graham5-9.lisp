f50b4e0d2b3f3955c45931dce304f2ed0ffe6fa5
youralien <Ryan.Louie@students.olin.edu>
2017-10-13 02:13:17 -0500

    Part B of 5-9 too!

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -1,40 +1,37 @@
 (defun shortest-path (start end net)
   (catch 'abort
-    (bfs end (list (list start)) net)))
-
-;;;(defun bfs (end queue seen net)
-;;;  (if (empty-queue-p queue)
-;;;    nil
-;;;    (let ((path (car queue)))
-;;;      (let ((node (car path)))
-;;;        (cond ((eql node end) (reverse path))
-;;;              ; ((not (null (member node seen))) (throw 'abort nil))
-;;;              (t (let ((nextpaths (new-paths path node net))
-;;;                       (newqueue (cdr queue)))
-;;;                   (dolist (npath nextpaths path)                   
-;;;                     (if (null (every (lambda (n) (member n seen)) npath)) 
-;;;                         ; (throw 'abort nil)
-;;;                         (setq newqueue (append newqueue (list npath)))
-;;;                       (setq newqueue (append newqueue (list npath)))))
-;;;                   (format t "~A" newqueue)
-;;;                   (bfs end newqueue (append seen (list node)) net))))))))
+         (bfs end (list (list start)) net)))
 
 (defun bfs (end queue net)
   (if (empty-queue-p queue)
       nil
-    (let ((path (car queue)))
-      (let ((node (car path)))
-        (if (eql node end)
-            (reverse path)
-          (let ((newpaths (remove-if-not #'only-unique-elements (new-paths path node net))))
-            (format t "~A" newpaths)
-            (dolist (npath newpaths)
-              (when (eql end (car npath))
-                (throw 'abort (reverse npath))))
-            (bfs end
-                 (append (cdr queue)
-                         newpaths)
-                 net)))))))
+    (let* ((path (car queue))
+           (node (car path))
+           (newpaths (remove-if-not #'only-unique-elements (new-paths path node net))))
+      (dolist (npath newpaths)
+        (when (eql end (car npath))
+          (throw 'abort (reverse npath))))
+      (bfs end
+           (append (cdr queue)
+                   newpaths)
+           net))))
+
+(defun shortest-path (start end net)
+  (bfs end (list (list start)) net))
+
+(defun bfs (end queue net)
+  (if (empty-queue-p queue)
+      nil
+    (let* ((path (car queue))
+           (node (car path))
+           (newpaths (remove-if-not #'only-unique-elements (new-paths path node net))))
+      (dolist (npath newpaths)
+        (when (eql end (car npath))
+          (return-from bfs (reverse npath))))
+      (bfs end
+           (append (cdr queue)
+                   newpaths)
+           net))))
 
 (defun only-one (sym lst)
   (null (member sym (cdr (member sym lst)))))
@@ -43,15 +40,12 @@
   (every (lambda (sym) (only-one sym lst)) lst))
 
 (define-test only-unique-elements
-  (assert-true (only-unique-elements '(A B C)))
-  (assert-false (only-unique-elements '(A B A)))
-  (assert-false (only-unique-elements '(A B A B)))
+    (assert-true (only-unique-elements '(A B C)))
+    (assert-false (only-unique-elements '(A B A)))
+    (assert-false (only-unique-elements '(A B A B)))
   )
-
-(defun car-is-goal (goal path)
-  (eql goal (car path)))
 
 (defun new-paths (path node net)
   (mapcar #'(lambda (n)
-    (cons n path))
-  (cdr (assoc node net))))
+              (cons n path))
+    (cdr (assoc node net))))
