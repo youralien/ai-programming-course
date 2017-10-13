f154bcae8cdcaef42ccad24d7510727b41bc1016
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-13 15:06:28 -0500

    fixed no-internal-cyclic-loops, 11 of 13 tests passed

--- ex/graham3-9.lisp
+++ ex/graham3-9.lisp
@@ -12,57 +12,31 @@
                                                    nextstates))
                             (cdr stack))))
     ; (format t "stack ~A ~C" stack #\linefeed)
+    (format t "nextpaths ~A ~C" nextpaths #\linefeed)
     (if (null stack)
         bestpath
       (if (funcall pred (car currpath))
           (when (> (length currpath) (length bestpath))
-              (dfs (cdr stack) currpath pred gen))
+              (dfs nextpaths currpath pred gen))
         (dfs nextpaths bestpath pred gen)))))
 
 
+(defun only-one-internally (sym lst)
+  (let* ((first-tail (member sym lst))
+         (second-tail (member sym (cdr first-tail))))
+    (if (eql (length first-tail) (length lst))
+        (null (cdr second-tail))
+        (null second-tail))))
+
 (defun no-internal-cyclic-loops (lst)
-  (every (lambda (sym) (null (member sym (cdr (member sym (cdr lst)))))) lst))
+  (every (lambda (sym)
+           (only-one-internally sym lst))
+         lst))
 
 (define-test no-internal-cyclic-loops
     (assert-true (no-internal-cyclic-loops '(A B C)))
     (assert-true (no-internal-cyclic-loops '(A B A)))
     (assert-false (no-internal-cyclic-loops '(A B A B)))
-    (assert-false (no-internal-cyclic-loops '(A B C B)'))
+    (assert-false (no-internal-cyclic-loops '(A B C B)))
+    (assert-false (no-internal-cyclic-loops '(B C B A)))
   )
-
-
-#|
-LONGEST-PATH: (LONGEST-PATH 'A 'A '((A B) (B A C))) failed: 
-Expected (A B A) but saw (A)
-LONGEST-PATH: (LONGEST-PATH 'A 'F
-                            '((A B C A) (B C D) (C E A) (D E F)
-                              (E D F))) failed: 
-Expected (A B C E D F) but saw NIL
-LONGEST-PATH: (LONGEST-PATH 'A 'A
-                            '((A B C A) (B C D) (C E A) (D E F)
-                              (E D F))) failed: 
-Expected (A B C A) but saw (A)
-LONGEST-PATH: (LONGEST-PATH 'A 'A '((A A B) (B C))) failed: 
-Expected (A A) but saw (A)
-LONGEST-PATH: (LONGEST-PATH 'A 'A '((A B A) (B C))) failed: 
-Expected (A A) but saw (A)
-LONGEST-PATH: 8 assertions passed, 5 failed.
-
--------
-
-LONGEST-PATH: (LONGEST-PATH 'A 'F
-                            '((A B C A) (B C D) (C E A) (D E F)
-                              (E D F))) failed: 
-Expected (A B C E D F) but saw NIL
-LONGEST-PATH: (LONGEST-PATH 'A 'A
-                            '((A B C A) (B C D) (C E A) (D E F)
-                              (E D F))) failed: 
-Expected (A B C A) but saw NIL
-LONGEST-PATH: (LONGEST-PATH 'A 'B '((A B) (B C) (C B))) failed: 
-Expected (A B) but saw (A B C B)
-LONGEST-PATH: (LONGEST-PATH 'A 'B '((A B C) (B C) (C B))) failed: 
-Expected (A C B) but saw NIL
-LONGEST-PATH: 9 assertions passed, 4 failed.
-
-#|
-
