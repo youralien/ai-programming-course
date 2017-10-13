8002510a092d899afbb9cbf17fec90b75158dfdb
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-13 15:40:52 -0500

    all tests passed for shortest-path

--- ex/graham3-9.lisp
+++ ex/graham3-9.lisp
@@ -12,12 +12,13 @@
                                                    nextstates))
                             (cdr stack))))
     ; (format t "stack ~A ~C" stack #\linefeed)
-    (format t "nextpaths ~A ~C" nextpaths #\linefeed)
+    ; (format t "nextpaths ~A ~C" nextpaths #\linefeed)
+    ; (format t "currpath ~A     bestpath ~A ~C" currpath bestpath #\linefeed)
     (if (null stack)
         bestpath
-      (if (funcall pred (car currpath))
-          (when (> (length currpath) (length bestpath))
-              (dfs nextpaths currpath pred gen))
+      (if (and (funcall pred (car currpath))
+                 (> (length currpath) (length bestpath)))
+          (dfs nextpaths currpath pred gen)
         (dfs nextpaths bestpath pred gen)))))
 
 
