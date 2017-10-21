f0c4172c2792839755d18f1ccbf5ffc4bcbaea08
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-21 18:38:16 -0500

    new-paths returns paths that dont have cycles

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -7,7 +7,7 @@
       nil
     (let* ((path (car queue))
            (node (car path))
-           (newpaths (remove-if-not #'only-unique-elements (new-paths path node net))))
+           (newpaths (new-paths path node net)))
       (dolist (npath newpaths)
         (when (eql end (car npath))
           (throw 'abort (reverse npath))))
@@ -24,7 +24,7 @@
       nil
     (let* ((path (car queue))
            (node (car path))
-           (newpaths (remove-if-not #'only-unique-elements (new-paths path node net))))
+           (newpaths (new-paths path node net)))
       (dolist (npath newpaths)
         (when (eql end (car npath))
           (return-from bfs (reverse npath))))
@@ -33,19 +33,8 @@
                    newpaths)
            net))))
 
-(defun only-one (sym lst)
-  (null (member sym (cdr (member sym lst)))))
-
-(defun only-unique-elements (lst)
-  (every (lambda (sym) (only-one sym lst)) lst))
-
-(define-test only-unique-elements
-    (assert-true (only-unique-elements '(A B C)))
-    (assert-false (only-unique-elements '(A B A)))
-    (assert-false (only-unique-elements '(A B A B)))
-  )
-
 (defun new-paths (path node net)
-  (mapcar #'(lambda (n)
-              (cons n path))
+  (mapcan #'(lambda (n)
+              (unless (member n path)
+                (list (cons n path))))
     (cdr (assoc node net))))
