2947b162931fde2294eed615b364e2efba59e1b4
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-21 19:22:13 -0500

    Responded to comments for bfs with throw

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -6,35 +6,36 @@
   (if (empty-queue-p queue)
       nil
     (let* ((path (car queue))
-           (node (car path))
-           (newpaths (new-paths path node net)))
-      (dolist (npath newpaths)
-        (when (eql end (car npath))
-          (throw 'abort (reverse npath))))
+           (node (car path)))
       (bfs end
            (append (cdr queue)
-                   newpaths)
+                   (new-paths path node end net))
            net))))
 
-(defun shortest-path (start end net)
-  (bfs end (list (list start)) net))
-
-(defun bfs (end queue net)
-  (if (empty-queue-p queue)
-      nil
-    (let* ((path (car queue))
-           (node (car path))
-           (newpaths (new-paths path node net)))
-      (dolist (npath newpaths)
-        (when (eql end (car npath))
-          (return-from bfs (reverse npath))))
-      (bfs end
-           (append (cdr queue)
-                   newpaths)
-           net))))
-
-(defun new-paths (path node net)
+(defun new-paths (path node end net)
   (mapcan #'(lambda (n)
               (unless (member n path)
-                (list (cons n path))))
+                (let ((npath (cons n path)))
+                  (if (eql n end)
+                      (throw 'abort (reverse npath))
+                    (list npath)))))
     (cdr (assoc node net))))
+
+; (defun shortest-path (start end net)
+;   (bfs end (list (list start)) net))
+
+; (defun bfs (end queue net)
+;   (if (empty-queue-p queue)
+;       nil
+;     (let* ((path (car queue))
+;            (node (car path))
+;            (newpaths (new-paths path node net)))
+;       (dolist (npath newpaths)
+;         (when (eql end (car npath))
+;           (return-from bfs (reverse npath))))
+;       (bfs end
+;            (append (cdr queue)
+;                    newpaths)
+;            net))))
+
+
