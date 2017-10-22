d1e769d25c87bc90a3641b37b8c9d27970b49634
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-22 16:50:26 -0500

    Fixed up generalize bfs

--- ex/generalized-bfs.lisp
+++ ex/generalized-bfs.lisp
@@ -7,22 +7,20 @@
   (if (empty-queue-p paths)
       nil
     (let* ((path (car paths))
-           (newpaths (new-paths path gen)))
-      (do* ((tmppaths newpaths (cdr tmppaths))
-            (npath (car tmppaths) (car tmppaths)))
-           ((null tmppaths))
-          (when (funcall pred (car npath))
-            (return-from bfs npath)))
-      ; (dolist (npath newpaths)
-      ;   (when (funcall pred (car npath))
-      ;     (return-from bfs npath)))
-      (bfs (append (cdr paths)
-                   newpaths)
-           pred
-           gen))))
+           (newpaths (new-paths path gen))
+           (found-end
+            (do* ((tmppaths newpaths (cdr tmppaths))
+                  (npath (car tmppaths) (car tmppaths)))
+                 ((or (null tmppaths)
+                      (funcall pred (car npath)))
+                  npath))))
+      (if found-end
+          found-end
+        (bfs (append (cdr paths) newpaths) pred gen)))))
     
 (defun new-paths (path gen)
   (mapcan #'(lambda (n)
-              (unless (member n path)
+              (if (member n path)
+                  nil
                 (list (cons n path))))
     (funcall gen path)))
