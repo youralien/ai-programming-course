83e8b7451056732b783599503f97d852eb798ba3
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-21 19:32:38 -0500

    Responded to comments for bfs without throw

--- ex/graham5-9.lisp
+++ ex/graham5-9.lisp
@@ -21,21 +21,25 @@
                     (list npath)))))
     (cdr (assoc node net))))
 
-; (defun shortest-path (start end net)
-;   (bfs end (list (list start)) net))
+(defun shortest-path (start end net)
+  (bfs end (list (list start)) net))
 
-; (defun bfs (end queue net)
-;   (if (empty-queue-p queue)
-;       nil
-;     (let* ((path (car queue))
-;            (node (car path))
-;            (newpaths (new-paths path node net)))
-;       (dolist (npath newpaths)
-;         (when (eql end (car npath))
-;           (return-from bfs (reverse npath))))
-;       (bfs end
-;            (append (cdr queue)
-;                    newpaths)
-;            net))))
+(defun bfs (end queue net)
+  (if (empty-queue-p queue)
+      nil
+    (let* ((path (car queue))
+           (node (car path))
+           (newpaths (new-paths path node net)))
+      (dolist (npath newpaths)
+        (when (eql end (car npath))
+          (return-from bfs (reverse npath))))
+      (bfs end
+           (append (cdr queue)
+                   newpaths)
+           net))))
 
-
+(defun new-paths (path node net)
+  (mapcan #'(lambda (n)
+              (unless (member n path)
+                (list (cons n path))))
+    (cdr (assoc node net))))
