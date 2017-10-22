1f49b4a764d326d8b9df462bd2e3c1c4872c05d6
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-22 09:29:26 -0500

    Replaced dolist with do in generalized bfs

--- ex/generalized-bfs.lisp
+++ ex/generalized-bfs.lisp
@@ -1,17 +1,21 @@
 (defun shortest-path (start end net)
-  (let ((paths (list (list start))))
-    (reverse (bfs paths 
-                  #'(lambda (state) (eql state end)) 
-                  #'(lambda (path) (cdr (assoc (car path) net)))))))
+  (reverse (bfs (list (list start)) 
+                 #'(lambda (state) (eql state end)) 
+                 #'(lambda (path) (cdr (assoc (car path) net))))))
 
 (defun bfs (paths pred gen)
   (if (empty-queue-p paths)
       nil
     (let* ((path (car paths))
            (newpaths (new-paths path gen)))
-      (dolist (npath newpaths)
-        (when (funcall pred (car npath))
-          (return-from bfs npath)))
+      (do* ((tmppaths newpaths (cdr tmppaths))
+            (npath (car tmppaths) (car tmppaths)))
+           ((null tmppaths))
+          (when (funcall pred (car npath))
+            (return-from bfs npath)))
+      ; (dolist (npath newpaths)
+      ;   (when (funcall pred (car npath))
+      ;     (return-from bfs npath)))
       (bfs (append (cdr paths)
                    newpaths)
            pred
