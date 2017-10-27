66cdbab04dedd2b60a1a9b82871dd61b34402804
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-26 21:13:21 -0500

    - works!

--- ex/graham7-2.lisp
+++ ex/graham7-2.lisp
@@ -1,8 +1,4 @@
-(defun output-expr (string)
-  (with-input-from-string (in string)
-	(format t "~A~C" (read in nil :eof) #\linefeed)))
-
 (defun map-stream (fn in)
-  (do ((expr (read in nil :#<>) (read in nil :#<>)))
-      ((eql expr :#<>))
-  	(funcall fn expr)))
+  (do ((expr (read in nil -) (read in nil -)))
+      ((eql expr -))
+    (funcall fn expr)))
