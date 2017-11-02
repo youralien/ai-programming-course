beae2dc47b4d5d0ac9107223cb44cfb96eece3d0
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-01 21:25:19 -0400

    cleanup more maxmin

--- ex/max-min.lisp
+++ ex/max-min.lisp
@@ -1,8 +1,7 @@
 (defun max-min (vec &key (start 0) (end (length vec)))
   (cond
     ((>= start end) (values nil nil))
-    (t (let ((elem (svref vec start)))
-         (find-max-min vec start end)))))
+    (t (find-max-min vec start end))))
 
 (defun find-max-min (vec start end &optional maximum minimum)
   (if (eql start end)
