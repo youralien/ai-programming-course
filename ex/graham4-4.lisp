8d0dd326e8a059760e59ca3930e89e1ec7c7d4a6
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-02 18:43:47 -0400

    uncomment bst

--- ex/graham4-4.lisp
+++ ex/graham4-4.lisp
@@ -1,10 +1,10 @@
 (load "/Users/ryan/quicklisp/local-projects/cs325/bst.lisp")
 
-; (defun bst-elements (bst)
-;   (cond ((null bst) nil)
-;         (t (append (bst-elements (node-r bst))
-;                    (list (node-elt bst))
-;                    (bst-elements (node-l bst))))))
+(defun bst-elements (bst)
+  (cond ((null bst) nil)
+        (t (append (bst-elements (node-r bst))
+                   (list (node-elt bst))
+                   (bst-elements (node-l bst))))))
 
 (defun bst-elements (bst)
   (let ((out nil))
