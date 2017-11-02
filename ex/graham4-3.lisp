6c3368122a2f943a418c9c218cf57095c1ce96d1
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-01 19:23:28 -0500

    3tree-clone cleaned up

--- ex/graham4-3.lisp
+++ ex/graham4-3.lisp
@@ -12,17 +12,9 @@
 ; use setf for the values, i.e. (setf (point-x point-instance) val)
 
 (defun 3tree-clone (tree)
-  ; (let ((clone (make-3tree :data tree-data
-  ;                          :left tree-left
-  ;                          :middle tree-middle
-  ;                          :right tree-right)))
   (if (null tree)
       nil
-    (let ((clone (copy-3tree tree)))
-      (make-3tree :data (3tree-data clone)
-                  :left (3tree-clone (3tree-left clone))
-                  :middle (3tree-clone (3tree-middle clone))
-                  :right (3tree-clone (3tree-right clone))))))
-                  ; :left (or (3tree-left clone) (3tree-clone (3tree-left clone)))
-                  ; :middle (or (3tree-middle clone) (3tree-clone (3tree-middle clone)))
-                  ; :right (or (3tree-right clone) (3tree-clone (3tree-right clone)))))))
+    (make-3tree :data (3tree-data tree)
+                :left (3tree-clone (3tree-left tree))
+                :middle (3tree-clone (3tree-middle tree))
+                :right (3tree-clone (3tree-right tree)))))
