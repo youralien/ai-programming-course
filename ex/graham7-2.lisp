51cec63b5a7f16f06c82efcbd32d05b17910988b
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-01 21:28:48 -0400

    replace - with lambda for map-stream

--- ex/graham7-2.lisp
+++ ex/graham7-2.lisp
@@ -1,4 +1,9 @@
+(defun map-file (fn name)
+  (with-open-file (str name :direction :input)
+    (map-stream fn str)))
+
 (defun map-stream (fn in)
-  (do ((expr (read in nil -) (read in nil -)))
-      ((eql expr -))
-    (funcall fn expr)))
+  (let ((obj (lambda (x) x)))
+    (do ((expr (read in nil obj) (read in nil obj)))
+      ((eql expr obj))
+      (funcall fn expr))))
