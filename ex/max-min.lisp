3c8e22ac81b575874f680d406f139a0748a8a9ce
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-02 18:22:17 -0400

    max-min ready for resubmission

--- ex/max-min.lisp
+++ ex/max-min.lisp
@@ -4,19 +4,11 @@
     (t (find-max-min vec start end))))
 
 (defun find-max-min (vec start end &key maximum minimum)
-  (if (eql start end)
-    	(values maximum minimum)
-	  (let ((elem (svref vec start)))
-	    (find-max-min vec
-	                  (1+ start)
-	                  end
-	               	  :maximum (if (null maximum) elem (max maximum elem))
-	                  :minimum (if (null minimum) elem (min minimum elem))))))
-
-; (defun find-max-min (vec start end)
-;   (if (eql start end)
-;       nil
-; 	  (let ((elem (svref vec start)))
-; 	    (multiple-value-bind (maximum minimum) (find-max-min vec (1+ start) end)
-; 				(values (max elem (or maximum elem))
-; 	           		(min elem (or minimum elem)))))))
+  (if (= start end)
+      (values maximum minimum)
+    (let ((elem (svref vec start)))
+      (find-max-min vec
+                    (1+ start)
+                    end
+                    :maximum (max (or maximum elem) elem)
+                    :minimum (min (or minimum elem) elem)))))
