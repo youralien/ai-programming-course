54b707d942e9a78132b3dfbb38a845cde63ed3b4
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-03 12:43:09 -0400

    ?contains passes

--- ex/pattern-matcher.lisp
+++ ex/pattern-matcher.lisp
@@ -25,19 +25,33 @@
   (destructuring-bind (sub-pattern function-name . args) x
     (match-p sub-pattern (apply function-name (cons y args)) lsts)))
 
+
 (defun ?contains (x y lsts)
   (do ((expr y (if (atom expr) nil (cdr expr)))
-       (total nil
+       (total (match-p (car x) y)
         (cond
-          ((atom expr) (append (match-p (car x) expr) total))
+          ((atom expr) total)
           ((atom (car expr))
-               (if (eql y expr)
-                   (append (match-p (car x) (car expr)) (match-p (car x) expr) total)
-                 (append (match-p (car x) (car expr)) total)))
-          (t (append (match-p (car x) expr) total
-                     (?contains x (car expr) lsts))))))
-      ((null expr) (or total (match-p (car x) y)))))
+             (append (match-p (car x) (car expr)) total))
+          (t (append (?contains x (car expr) lsts) total)))))
+      ((null expr) total)))
 
+;;; contains, long form
+; (defun ?contains (x y lsts)
+;   (do ((expr y (if (atom expr) nil (cdr expr)))
+;        (total nil
+;         (cond
+;           ((atom expr)
+;            (append (match-p (car x) expr) total))
+;           ((atom (car expr))
+;            (if (eql y expr)
+;                (append (match-p (car x) (car expr)) (match-p (car x) expr) total)
+;              (append (match-p (car x) (car expr)) total)))
+;           (t (append (match-p (car x) expr) total
+;                      (?contains x (car expr) lsts))))))
+;       ((null expr) (or total (match-p (car x) y)))))
+
+;;; contains, with subexp
 ; (defun ?contains (x y lsts)
 ;   (mapcan #'(lambda (expr)
 ;              (match-p (car x) expr))
