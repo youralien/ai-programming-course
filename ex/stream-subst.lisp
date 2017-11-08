e2f8eff549fed77911081da2589d6ffa6bf44b6a
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-07 23:54:59 -0600

    wildcard testcases for stream-subst

---
+++ ex/stream-subst.lisp
@@ -0,0 +1,42 @@
+(in-package :cs325-user)
+
+;;; From Paul Graham's ANSI Common Lisp
+;;; Figure 7.2, Page 129
+;;;
+;;; Requires fixed version of buf.lisp
+
+(defun file-subst (old new file1 file2)
+  (with-open-file (in file1 :direction :input)
+     (with-open-file (out file2 :direction :output
+                                :if-exists :supersede)
+       (stream-subst old new in out))))
+
+(defun stream-subst (old new in out &key (wildcard #\+))
+  (let* ((pos 0)
+         (len (length old))
+         (buf (new-buf len))
+         (from-buf nil))
+    (do ((c (read-char in nil :eof)
+            (or (setf from-buf (buf-next buf))
+                (read-char in nil :eof))))
+        ((eql c :eof))
+      (cond ((or (char= c (char old pos)) (char= wildcard (char old pos)))
+             (incf pos)
+             (cond ((= pos len)            ; 3
+                    (princ new out)
+                    (setf pos 0)
+                    (buf-clear buf))
+                   ((not from-buf)         ; 2
+                    (buf-insert c buf))))
+            ((zerop pos)                   ; 1
+             (princ c out)
+             (when from-buf
+               (buf-pop buf)
+               (buf-reset buf)))
+            (t                             ; 4
+             (unless from-buf
+               (buf-insert c buf))
+             (princ (buf-pop buf) out)
+             (buf-reset buf)
+             (setf pos 0))))
+    (buf-flush buf out)))
