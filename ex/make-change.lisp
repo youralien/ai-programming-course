72e5265a2cd8f724a9a8a51a9d15a234e5e9a584
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-12 00:56:11 -0600

    much more natural recursive definition

--- ex/make-change.lisp
+++ ex/make-change.lisp
@@ -1,36 +1,50 @@
 ; some doesn't return the index, so maybe not useful
+; I could imagine making a hash table to store the indexes
+; of each of the coins.  Then using some, I could find the remainder,
+; keep a tally of the index, and recurse solving make-change for the
+; the remaining val
+(defun make-change (val &optional (coins '(25 10 5 1)))
+  (let ((coin-usage-ht (make-hash-table :size (length coins))))
+    (dolist (coin coins)
+      (setf (gethash coin coin-usage-ht) 0))
+    (subtract-and-record val coins coin-usage-ht)))
+    
 
-; (defun make-change (val &optional (coins '(25 10 5 1)))
-;   (let ((remain (some #'(lambda (coin)
-;                          (if (> val coin)
-;                              (- val coin)
-;                             nil))
-;                       coins)))
-;     (print remain
-
+(defun subtract-and-record (val coins coin-usage-ht)
+  (let ((use-coin (some #'(lambda (coin)
+                         (if (>= val coin)
+                             coin
+                            nil))
+                      coins)))
+    (cond
+      ((null use-coin)
+       (values-list
+         (mapcar #'(lambda (coin)
+                     (gethash coin coin-usage-ht))
+                 coins)))
+      (t (incf (gethash use-coin coin-usage-ht))
+         (subtract-and-record (- val use-coin)
+                              coins
+                              coin-usage-ht)))))
+    
 
 ; i don't like this I am brain dead
-(defun make-change (val &optional (coins '(25 10 5 1)))
-  (if (zerop val)
-      (values 0 0 0 0)
-    (do* ((i 0)
-          (coin-length (length coins))
-          (change val)
-          (remain (- change (nth i coins))
-                  (- change (nth i coins)))
-          (coin-counts (make-list coin-length :initial-element 0)))
-         ((= remain 0) 
-          (incf (nth i coin-counts))
-          (values-list coin-counts))
-     ; (format t "~C----~C" #\linefeed #\linefeed)
-     ; (print remain)
-     ; (print coin-counts)
-     ; (print change)
-     ; (print i)
-      (cond
-        ((not (minusp remain))
-         (setf change remain)
-         (incf (nth i coin-counts)))
-        (t
-         (unless (= i (1- coin-length))
-           (incf i)))))))
+; (defun make-change (val &optional (coins '(25 10 5 1)))
+;   (if (zerop val)
+;       (values-list (make-list (length coins) :initial-element 0))
+;     (do* ((i 0)
+;           (coin-length (length coins))
+;           (change val)
+;           (remain (- change (nth i coins))
+;                   (- change (nth i coins)))
+;           (coin-counts (make-list coin-length :initial-element 0)))
+;          ((= remain 0) 
+;           (incf (nth i coin-counts))
+;           (values-list coin-counts))
+;       (cond
+;         ((not (minusp remain))
+;          (setf change remain)
+;          (incf (nth i coin-counts)))
+;         (t
+;          (unless (= i (1- coin-length))
+;            (incf i)))))))
