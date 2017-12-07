c0c513e27dd9770d32b332920ecf524b42323e16
youralien <ryanlouie2021@u.northwestern.edu>
2017-12-07 00:11:11 -0600

    resubmitted longest-path

--- ex/graham3-9.lisp
+++ ex/graham3-9.lisp
@@ -1,8 +1,8 @@
-(defun longest-path (start end net)
-  (reverse (dfs (list (list start))
-                nil
-                (lambda (n) (eql n end))
-                (lambda (path) (cdr (assoc (car path) net))))))
+; (defun longest-path (start end net)
+;   (reverse (dfs (list (list start))
+;                 nil
+;                 (lambda (n) (eql n end))
+;                 (lambda (path) (cdr (assoc (car path) net))))))
 
 ; (defun dfs (stack bestpath pred gen)
 ;   (let* ((currpath (car stack))
@@ -18,9 +18,25 @@
 ;        (dfs nextpaths currpath pred gen))
 ;       (t (dfs nextpaths bestpath pred gen)))))
 
-; (defun dfs (bestpath pred gen)
-;   )
+(defun longest-path (start end net)
+  (reverse (dfs start
+                (lambda (n) (eql n end))
+                (lambda (path) (cdr (assoc (car path) net))))))
+
+(defun dfs (start pred gen)
+  (do ((adjacents (funcall gen (list start))
+                  (cdr adjacents))
+       (best-path-so-far
+         (cond ((funcall pred start) (list start))
+               (t nil))
+         (longer-of best-path-so-far
+                    (path-thru-node-to-end (car adjacents) (list start) pred gen))))
+      ((null adjacents) best-path-so-far)))
       
+(defun longer-of (path1 path2)
+  (cond ((> (length path2) (length path1)) path2)
+        (t path1)))
+
 ; (defun foo (path pred gen)
 ;   (do* ((n-to-explore (funcall gen path) (cdr n-to-explore))
 ;        (n (car n-to-explore) (car n-to-explore)))
@@ -31,36 +47,39 @@
 ;         (return (foo (cons n path) pred gen))))))
 
 (defun path-thru-node-to-end (n path pred gen)
-  (format t "~Cn: ~A~C" #\linefeed n #\linefeed)
-  (format t "path: ~A~C" path #\linefeed)
-  (if (eql n 'c)
-      (format t "wtf: ~A~C" (funcall pred n) #\linefeed))
-  (if (and (not (null path)) (funcall pred n))
-      (progn
-        (format t "entered but not return")
-        (cons n path))
+  (if (and (not (null path)) (funcall pred n)) ; there is a path and we've found the end
+      (cons n path)
     (unless (member n path) ; ignore cycles
       (do* ((adjacents (funcall gen (cons n path)) (cdr adjacents))
-            (potential-path (path-thru-node-to-end (car adjacents)
-                                       (cons n path)
-                                       pred
-                                       gen)))
+            (potential-path (path-thru-node-to-end (car adjacents) (cons n path) pred gen)
+                            (path-thru-node-to-end (car adjacents) (cons n path) pred gen)))
            ((or (null adjacents) (not (null potential-path)))
-            potential-path)
-        (format t "adjs: ~A~C" adjacents #\linefeed)
-        (format t "adj: ~A~C" (car adjacents) #\linefeed)
-        (format t "pot: ~A~C" potential-path #\linefeed)))))
+            potential-path)))))
 
-(defun a-path (start end net)
+; (defun path-thru-node-to-end (n path pred gen)
+;   (cond ((and (not (null path)) (funcall pred n)) ; there is a path and we've found the end
+;          (cons n path))
+;         ((member n path) ; ignore cycles
+;          (if (null ))
+;              (list path)
+;             nil))
+;         (t (do* ((adjacents (funcall gen (cons n path)) (cdr adjacents))
+;                  (potential-path (path-thru-node-to-end (car adjacents) (cons n path) pred gen)
+;                                  (path-thru-node-to-end (car adjacents) (cons n path) pred gen)))
+;                 ((or (null adjacents) (not (null potential-path)))
+;                  potential-path)))))
+
+  (defun a-path (start end net)
   (path-thru-node-to-end start
                          nil
                          (lambda (n) (eql n end))
                          (lambda (path) (cdr (assoc (car path) net)))))
 
 (define-test a-path
-  ; (assert-equal '(c b a) (a-path 'a 'c '((a b) (b c))))
+  (assert-equal '(c b a) (a-path 'a 'c '((a b) (b c))))
   (assert-equal '(c b a) (a-path 'a 'c '((a b) (b a c))))
-  ; (assert-equal '(a b a) (a-path 'a 'a '((a b) (b a c))))
-  ; (assert-equal nil (a-path 'a 'c '((a b) (b a) (c))))
-  ; (assert-equal '(f b a) (a-path 'a 'f '((a b c) (b f) (c d) (d e) (e f))))
+  (assert-equal '(a b a) (a-path 'a 'a '((a b) (b a c))))
+  (assert-equal nil (a-path 'a 'c '((a b) (b a) (c))))
+  (assert-equal '(a) (a-path 'A 'A '((A B) (B C)))) ; fixed up top
+  (assert-equal '(f b a) (a-path 'a 'f '((a b c) (b f) (c d) (d e) (e f))))
 )
